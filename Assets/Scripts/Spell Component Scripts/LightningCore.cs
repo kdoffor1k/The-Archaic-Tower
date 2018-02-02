a1026e6ee5156a724bcdb7cae47d7be65e76cd1b
Dor Hananel <dor.hanannel@gmail.com>
2018-02-02 18:18:58 -0500

    temp

---
+++ Assets/Scripts/Spell Component Scripts/LightningCore.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningCore : SpellCore {
+
+	public void doOnCast()
+	{
+		base.doOnCast();
+		RaycastHit hit;
+		if (Physics.Raycast(transform.position, transform.up, out hit))
+		{
+			Resources.Load("ArrowPrefab", typeof(GameObject));
+		}
+
+	}
+}
